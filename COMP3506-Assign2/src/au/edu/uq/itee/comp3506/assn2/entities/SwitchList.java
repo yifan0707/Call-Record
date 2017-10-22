f7e08794620186684d8e68d8ca1f17685918d9de
Yifan Lu <lvan0707@outlook.com>
2017-10-23 03:04:46 +1000

    quick commit

--- COMP3506-Assign2/src/au/edu/uq/itee/comp3506/assn2/entities/SwitchList.java
+++ COMP3506-Assign2/src/au/edu/uq/itee/comp3506/assn2/entities/SwitchList.java
@@ -1,10 +1,13 @@
 package au.edu.uq.itee.comp3506.assn2.entities;
 
 public class SwitchList {
-	private SwitchElement[] elements;
-	private int capacity;
+	private SwitchElement[] elements;		//List that contains all the switch element
+	private int capacity;					//list capacity
 
-	
+	/**
+	 * constructor of the switchList 
+	 * @param capacity
+	 */
 	public SwitchList(int capacity){
 		this.capacity=capacity;
 		elements=new SwitchElement[capacity];
